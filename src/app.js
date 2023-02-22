cabbc74b4f78b18b658bcebfaad3ce0187fa6cee
Will <110594293+willro93@users.noreply.github.com>
2023-02-22 09:58:59 -0600

    cuarto-desafio-romero

--- src/app.js
+++ src/app.js
@@ -19,3 +19,23 @@ app.listen(8080, () => console.log("server up!"));
 app.use("/api/products", productsRouter);
 
 app.use("/api/carts", cartsRouter);
+
+//NOTE - ruta handlebars
+app.get("/", (req, res) => {
+  res.render();
+});
+app.get("/realtimeproducts", (req, res) => {
+  res.render("realtimeProducts", {
+    title: "lista de Productos",
+    producto1: "Shampoo Sólido de Romero",
+    producto2: "Acondicionador Sólido de Canéla",
+    producto3: "Shampoo Sólido de Linaza",
+    producto4: "Shampoo Sólido de Carbón",
+    producto5: "Shampoo Sólido de Café",
+    producto6: "Shampoo Sólido de Cola de caballo",
+    producto7: "Shampoo Sólido de Avena",
+    producto8: "Acondicionador Sólido de Coco",
+    producto9: "Shampoo Sólido de Chocolate",
+    producto10: "Shampoo Sólido de Pétalos de rosa",
+  });
+});
